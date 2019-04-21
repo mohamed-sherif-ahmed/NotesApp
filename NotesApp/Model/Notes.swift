ab14879d0e3cfbee42c2eeb0bb6b9b2c2f24cb08
Mohamed Sherif <msahmed652@gmail.com>
2019-04-21 19:38:30 +0200

    added Note class

---
+++ NotesApp/Model/Notes.swift
@@ -0,0 +1,14 @@
+//
+//  Notes.swift
+//  NotesApp
+//
+//  Created by Mohamed Sherif on 3/25/19.
+//  Copyright © 2019 Mohamed Sherif. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Note: NSManagedObject{
+    
+}
